8dfc41656bef0161b486e5fbfdbc74d0316514f7
Binh2423 <92388486+Binh2423@users.noreply.github.com>
2023-07-24 10:21:53 +0700

    Add files via upload

---
+++ NguyenPhamDiemQuynh_2180605554/De02/frmSanPham.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace De02
+{
+    partial class frmSanPham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.txtMaSP = new System.Windows.Forms.TextBox();
+            this.txtTenSP = new System.Windows.Forms.TextBox();
+            this.dtNgayNhap = new System.Windows.Forms.DateTimePicker();
+            this.cboLoaiSP = new System.Windows.Forms.ComboBox();
+            this.lvSanpham = new System.Windows.Forms.ListView();
+            this.MaSp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TenSP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NgayNhap = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LoaiSP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btTim = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btLuu = new System.Windows.Forms.Button();
+            this.btKLuu = new System.Windows.Forms.Button();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(327, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(393, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH MỤC SẢN PHẨM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(734, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã SP :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(734, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên SP :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(706, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ngày nhập :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(725, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Loại SP :";
+            // 
+            // txtTim
+            // 
+            this.txtTim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTim.Location = new System.Drawing.Point(28, 59);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(180, 30);
+            this.txtTim.TabIndex = 5;
+            // 
+            // txtMaSP
+            // 
+            this.txtMaSP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaSP.Location = new System.Drawing.Point(834, 109);
+            this.txtMaSP.Name = "txtMaSP";
+            this.txtMaSP.Size = new System.Drawing.Size(261, 30);
+            this.txtMaSP.TabIndex = 6;
+            // 
+            // txtTenSP
+            // 
+            this.txtTenSP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenSP.Location = new System.Drawing.Point(834, 170);
+            this.txtTenSP.Name = "txtTenSP";
+            this.txtTenSP.Size = new System.Drawing.Size(261, 30);
+            this.txtTenSP.TabIndex = 7;
+            // 
+            // dtNgayNhap
+            // 
+            this.dtNgayNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtNgayNhap.Location = new System.Drawing.Point(835, 229);
+            this.dtNgayNhap.Name = "dtNgayNhap";
+            this.dtNgayNhap.Size = new System.Drawing.Size(260, 30);
+            this.dtNgayNhap.TabIndex = 8;
+            // 
+            // cboLoaiSP
+            // 
+            this.cboLoaiSP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboLoaiSP.FormattingEnabled = true;
+            this.cboLoaiSP.Location = new System.Drawing.Point(835, 283);
+            this.cboLoaiSP.Name = "cboLoaiSP";
+            this.cboLoaiSP.Size = new System.Drawing.Size(260, 33);
+            this.cboLoaiSP.TabIndex = 9;
+            // 
+            // lvSanpham
+            // 
+            this.lvSanpham.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.MaSp,
+            this.TenSP,
+            this.NgayNhap,
+            this.LoaiSP});
+            this.lvSanpham.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvSanpham.FullRowSelect = true;
+            this.lvSanpham.GridLines = true;
+            this.lvSanpham.HideSelection = false;
+            this.lvSanpham.Location = new System.Drawing.Point(20, 109);
+            this.lvSanpham.Name = "lvSanpham";
+            this.lvSanpham.Size = new System.Drawing.Size(680, 336);
+            this.lvSanpham.TabIndex = 10;
+            this.lvSanpham.UseCompatibleStateImageBehavior = false;
+            this.lvSanpham.View = System.Windows.Forms.View.Details;
+            this.lvSanpham.SelectedIndexChanged += new System.EventHandler(this.lvSanpham_SelectedIndexChanged);
+            // 
+            // MaSp
+            // 
+            this.MaSp.Text = "Mã SP";
+            this.MaSp.Width = 104;
+            // 
+            // TenSP
+            // 
+            this.TenSP.Text = "Tên sản phẩm";
+            this.TenSP.Width = 221;
+            // 
+            // NgayNhap
+            // 
+            this.NgayNhap.Text = "Ngày nhập";
+            this.NgayNhap.Width = 211;
+            // 
+            // LoaiSP
+            // 
+            this.LoaiSP.Text = "Loại SP";
+            this.LoaiSP.Width = 157;
+            // 
+            // btTim
+            // 
+            this.btTim.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btTim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTim.Location = new System.Drawing.Point(240, 52);
+            this.btTim.Name = "btTim";
+            this.btTim.Size = new System.Drawing.Size(93, 37);
+            this.btTim.TabIndex = 11;
+            this.btTim.Text = "Tìm";
+            this.btTim.UseVisualStyleBackColor = false;
+            this.btTim.Click += new System.EventHandler(this.btTim_Click);
+            // 
+            // btThem
+            // 
+            this.btThem.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThem.Location = new System.Drawing.Point(135, 470);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(93, 37);
+            this.btThem.TabIndex = 12;
+            this.btThem.Text = "Thêm";
+            this.btThem.UseVisualStyleBackColor = false;
+            this.btThem.Click += new System.EventHandler(this.btThem_Click);
+            // 
+            // btSua
+            // 
+            this.btSua.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSua.Location = new System.Drawing.Point(290, 470);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(93, 37);
+            this.btSua.TabIndex = 13;
+            this.btSua.Text = "Sửa";
+            this.btSua.UseVisualStyleBackColor = false;
+            this.btSua.Click += new System.EventHandler(this.btSua_Click);
+            // 
+            // btXoa
+            // 
+            this.btXoa.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btXoa.Location = new System.Drawing.Point(445, 470);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(93, 37);
+            this.btXoa.TabIndex = 14;
+            this.btXoa.Text = "Xóa";
+            this.btXoa.UseVisualStyleBackColor = false;
+            this.btXoa.Click += new System.EventHandler(this.btXoa_Click);
+            // 
+            // btLuu
+            // 
+            this.btLuu.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLuu.Location = new System.Drawing.Point(618, 470);
+            this.btLuu.Name = "btLuu";
+            this.btLuu.Size = new System.Drawing.Size(93, 37);
+            this.btLuu.TabIndex = 15;
+            this.btLuu.Text = "Lưu";
+            this.btLuu.UseVisualStyleBackColor = false;
+            this.btLuu.Click += new System.EventHandler(this.btLuu_Click);
+            // 
+            // btKLuu
+            // 
+            this.btKLuu.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btKLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btKLuu.Location = new System.Drawing.Point(776, 470);
+            this.btKLuu.Name = "btKLuu";
+            this.btKLuu.Size = new System.Drawing.Size(93, 37);
+            this.btKLuu.TabIndex = 16;
+            this.btKLuu.Text = "K.Lưu";
+            this.btKLuu.UseVisualStyleBackColor = false;
+            this.btKLuu.Click += new System.EventHandler(this.btKLuu_Click);
+            // 
+            // btThoat
+            // 
+            this.btThoat.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThoat.Location = new System.Drawing.Point(953, 470);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(93, 37);
+            this.btThoat.TabIndex = 17;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = false;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // frmSanPham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 533);
+            this.Controls.Add(this.btThoat);
+            this.Controls.Add(this.btKLuu);
+            this.Controls.Add(this.btLuu);
+            this.Controls.Add(this.btXoa);
+            this.Controls.Add(this.btSua);
+            this.Controls.Add(this.btThem);
+            this.Controls.Add(this.btTim);
+            this.Controls.Add(this.lvSanpham);
+            this.Controls.Add(this.cboLoaiSP);
+            this.Controls.Add(this.dtNgayNhap);
+            this.Controls.Add(this.txtTenSP);
+            this.Controls.Add(this.txtMaSP);
+            this.Controls.Add(this.txtTim);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSanPham";
+            this.Text = "QUAN LY SAN PHAM";
+            this.Load += new System.EventHandler(this.frmSanPham_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.TextBox txtMaSP;
+        private System.Windows.Forms.TextBox txtTenSP;
+        private System.Windows.Forms.DateTimePicker dtNgayNhap;
+        private System.Windows.Forms.ComboBox cboLoaiSP;
+        private System.Windows.Forms.ListView lvSanpham;
+        private System.Windows.Forms.ColumnHeader MaSp;
+        private System.Windows.Forms.ColumnHeader TenSP;
+        private System.Windows.Forms.ColumnHeader NgayNhap;
+        private System.Windows.Forms.ColumnHeader LoaiSP;
+        private System.Windows.Forms.Button btTim;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btLuu;
+        private System.Windows.Forms.Button btKLuu;
+        private System.Windows.Forms.Button btThoat;
+    }
+}
+
---
+++ NguyenPhamDiemQuynh_2180605554/De02/frmSanPham.cs
@@ -0,0 +1,145 @@
+﻿using De02.database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace De02
+{
+    public partial class frmSanPham : Form
+    {
+        private QLSanpham context = new QLSanpham();
+        public frmSanPham()
+        {
+            InitializeComponent();
+        }
+
+        private void btThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btKLuu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLuu_Click(object sender, EventArgs e)
+        {
+           context.SaveChanges();
+        }
+
+        
+
+        private void frmSanPham_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                List<LoaiSP> listLoaiSP = context.LoaiSP.ToList();
+                List<Sanpham> listSP = context.Sanpham.ToList();
+                FillComboBox(listLoaiSP);
+                BindGrid(listSP);
+                btKLuu.Enabled = false;
+                btLuu.Enabled = false;
+
+            }catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);  
+            } 
+        }
+
+        private void FillComboBox(List<LoaiSP> list)
+        {
+            cboLoaiSP.DataSource = list;
+            cboLoaiSP.DisplayMember = "TenLoai";
+            cboLoaiSP.ValueMember = "MaLoai";
+        }
+        private void BindGrid(List<Sanpham> list)
+        {
+            lvSanpham.Items.Clear();
+            foreach (Sanpham sp in list)
+            {
+                ListViewItem item = lvSanpham.Items.Add(sp.MaSP);
+                item.SubItems.Add(sp.TenSP);
+                item.SubItems.Add(sp.Ngaynhap.ToString());
+                item.SubItems.Add(sp.LoaiSP.TenLoai.ToString());
+
+            }
+        }
+
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            Sanpham sp = new Sanpham()
+            {
+                MaSP = txtMaSP.Text,
+                TenSP = txtTenSP.Text,
+                Ngaynhap = dtNgayNhap.Value,
+                MaLoai = (cboLoaiSP.SelectedItem as LoaiSP).MaLoai,
+            };
+            context.Sanpham.Add(sp);
+            context.SaveChanges();
+            lvSanpham.Items.Clear();
+            BindGrid(context.Sanpham.ToList());
+            MessageBox.Show("Thêm dữ liệu thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btSua_Click(object sender, EventArgs e)
+        {
+           Sanpham sp = context.Sanpham.FirstOrDefault(p => p.MaLoai == txtMaSP.Text);
+            if (sp != null)
+            {
+                sp.TenSP = txtTenSP.Text;
+                sp.Ngaynhap = dtNgayNhap.Value;
+                sp.MaLoai = (cboLoaiSP.SelectedItem as LoaiSP).MaLoai;
+                context.SaveChanges();
+                lvSanpham.Items.Clear();
+                BindGrid(context.Sanpham.ToList());
+                
+                MessageBox.Show("Sửa dữ liệu thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show("Không tìm thấy sản phẩm", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            Sanpham sp = context.Sanpham.FirstOrDefault(p => p.MaSP == txtMaSP.Text);
+            if (sp != null) 
+            {
+                context.Sanpham.Remove(sp);
+                context.SaveChanges();
+                lvSanpham.Items.Clear();
+                BindGrid(context.Sanpham.ToList());
+                MessageBox.Show("Xóa dữ liệu thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show("Không tìm thấy sản phẩm", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+        }
+
+        private void btTim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvSanpham_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvSanpham.SelectedItems.Count > 0)
+            {
+                txtMaSP.Text = lvSanpham.SelectedItems[0].SubItems[0].Text;
+                txtTenSP.Text = lvSanpham.SelectedItems[0].SubItems[1].Text;
+                dtNgayNhap.Value = DateTime.Parse(lvSanpham.SelectedItems[0].SubItems[2].Text);
+                cboLoaiSP.Text = lvSanpham.SelectedItems[0].SubItems[3].Text;
+            }
+        }
+    }
+}
